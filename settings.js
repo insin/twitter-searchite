f45cb1b460719722b24becee4609edc45cb902d2
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2012-07-14 02:10:52 +0100

    Reorganised & commented settings
    - Added default search text

--- settings.js
+++ settings.js
@@ -1,16 +1,31 @@
 module.exports = {
+// Twitter
+  // App and auth settings are currently optional
   consumerKey: ''
 , consumerSecret: ''
 , accessToken: ''
 , accessTokenSecret: ''
-, search: ''
+
+// Poller
+  // Search text
+, search: '#nodejs OR Node.js'
+  // Wait time between Search API calls checking for new Tweets (seconds)
 , pollInterval: 60
+  // Should the server start the poller?
 , serverPoll: true
+  // Should the poller ignore Tweets which appear to be RTs?
+, ignoreRTs: true
+
+// Redis
 , redisPort: 6379
 , redisHost: '127.0.0.1'
 , redisDatabase: 0
+  // Password, or false if auth is nor required
 , redisAuth: false
+
+// Frontend
+  // Number of Tweets per page
 , tweetsPerPage: 50
-, ignoreRTs: true
+  // Wait time between XHR calls checking for new Tweets (seconds)
 , browserPollInterval: 30
 }
